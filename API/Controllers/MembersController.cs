12bf7abc5edce3db5bcbe00d53db0240f2d9f5b7
Rizq0 <joe_ethan2@hotmail.com>
2025-09-12 00:23:56 +0100

    feat: implement user authentication with JWT and create account/login endpoints

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,21 +1,19 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+{   
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
         {
             return Ok(await context.Users.ToListAsync());
         }
-
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
