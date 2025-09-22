61249acea7cb4d30fab1dbebd35c384d5f4c4833
Rizq0 <joe_ethan2@hotmail.com>
2025-09-23 00:15:58 +0100

    feat: add user authentication with login/logout functionality and user type definitions

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,15 +1,28 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {User} from '../../types/user';
+import {tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
 
   baseUrl = 'https://localhost:5001/api/'; // hardcoded for simplicity, ideally from environment
 
   login(credentials: any) {
-    return this.http.post(this.baseUrl + 'account/login', credentials);
+    return this.http.post<User>(this.baseUrl + 'account/login', credentials).pipe(tap(user => {
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.currentUser.set(user);
+      }
+    } ));
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
