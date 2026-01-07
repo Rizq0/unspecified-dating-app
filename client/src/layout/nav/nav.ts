87f8a2f91945a65371ba0531396dfc7324abd208
Rizq0 <joe_ethan2@hotmail.com>
2026-01-07 23:35:29 +0000

    feat: implement routing and navigation for home, members, lists, and messages

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,21 +1,23 @@
 import {Component, inject, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AccountService} from '../../core/services/account-service';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
   protected credentials: any = {};
 
   login() {
     this.accountService.login(this.credentials).subscribe({
       next: (response) => {
-        console.log('Login successful', response);
+        this.router.navigateByUrl('/members');
         this.credentials = {};
       },
       error: (error) => {
@@ -25,6 +27,7 @@ export class Nav {
   }
 
   logout() {
+    this.router.navigateByUrl('/');
     this.accountService.logout();
   }
 }
