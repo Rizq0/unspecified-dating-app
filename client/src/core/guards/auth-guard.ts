13cfbff7fd5783229f9d69c632d29ecab89f2245
Rizq0 <joe_ethan2@hotmail.com>
2026-01-09 11:49:56 +0000

    feat: restructure routing to include nested routes and enhance authentication guard functionality

--- client/src/core/guards/auth-guard.ts
+++ client/src/core/guards/auth-guard.ts
@@ -1,7 +1,7 @@
-import { CanActivateFn } from '@angular/router';
-import { inject } from '@angular/core';
-import { AccountService } from '../services/account-service';
-import { ToastService } from '../services/toast-service';
+import {CanActivateFn} from '@angular/router';
+import {inject} from '@angular/core';
+import {AccountService} from '../services/account-service';
+import {ToastService} from '../services/toast-service';
 
 export const authGuard: CanActivateFn = () => {
   const accountService = inject(AccountService);
